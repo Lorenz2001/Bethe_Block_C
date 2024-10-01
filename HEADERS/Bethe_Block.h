d40bf3a890b737f8e126b585fc5ce41a754e1e86
Lorenz2001 <pepa.lorenzo.01@gmail.com>
2024-10-01 12:44:37 +0200

    Esecuzione andata a buon fine, ora si tratta di sistemare la Bethe Block

--- HEADERS/Bethe_Block.h
+++ HEADERS/Bethe_Block.h
@@ -5,74 +5,57 @@
 
 #include "../HEADERS/material.h"
 
+enum CORRECTIONS { DENSITY_EFFECT = 0, SHELL_CORRECTION = 1 };
 
-
-enum CORRECTIONS {
-    DENSITY_EFFECT = 0,
-    SHELL_CORRECTION = 1
-};
-
-
-
-
-class Bethe_Block
-{
+class Bethe_Block {
 private:
-    //Bethe-Block Formula Constants
-    double _K;
-    //Detector MATERIAL
-    int _Z; 
-    int _A; 
-    double _zeta; // For density effect
-    double _I; //Minimum atom exitation energy
-    double _thick; //thickness of the detector
+  // Bethe-Block Formula Constants
+  double _K;
+  // Detector MATERIAL
+  int _Z;
+  int _A;
+  double _zeta;  // For density effect
+  double _I;     // Minimum atom exitation energy
+  double _thick; // thickness of the detector
 
+  // Corrections enables
+  bool _shell_corr;
+  bool _density_corr;
+  bool _global_enable_correction;
 
-    //Corrections enables
-    bool _shell_corr;
-    bool _density_corr;
-    bool _global_enable_correction;
+  // Functions
+  double _Compute_K();
 
+  // Bethe computations
+  // Corrections
+  bool _Enable_Correction(CORRECTIONS correction,
+                          bool enable); // valid locally (case by case)
+  double _Density_Effect(const double &beta, const double &gamma);
+  double _Shell_Correction(const double &beta, const double &gamma);
+  // Bethe terms
+  double _Log(const double &beta, const double &gamma);
+  double _Linear_z(const double &dE, const double &beta); //!TO IMPLEMENT
+  double _Linear_dE(const double &z, const double &beta); //!TO IMPLEMENT
+  // Other Functions
 
-    //Functions
-    double _Compute_K();
-
-        //Bethe computations
-            //Corrections
-    bool _Enable_Correction(CORRECTIONS correction, bool enable); // valid locally (case by case)
-    double _Density_Effect(const double& beta, const double& gamma);
-    double _Shell_Correction(const double& beta, const double& gamma);
-            //Bethe terms
-    double _Log(const double& beta, const double& gamma);
-    double _Linear_z(const double& dE, const double& beta);
-    double _Linear_dE(const double& z, const double& beta);
-            //Other Functions
-
-    double _Tmax(const double& M, const double& beta, const double& gamma);
-
-
-    
+  double _Tmax(const double &M, const double &beta, const double &gamma); //!TO MAKE BETTER
 
 public:
-    Bethe_Block(bool corrections = true, Material material = Material(),  double thickness = 0);
-    ~Bethe_Block();
+  Bethe_Block(bool corrections = true, Material material = Material(),
+              double thickness = 0);
+  ~Bethe_Block();
 
-    //Settings
-    void Set_Material(int Z, int A, double zeta, double I, double thickness);
-    void Set_Material(Material material, double thickness);
+  // Settings
+  void Set_Material(int Z, int A, double zeta, double I, double thickness);
+  void Set_Material(Material material, double thickness);
 
-    //GLOBAL CORRECTION ENABLE
-    
-    bool Enable_Correction(bool enable); //GLOBAL ENABLE or DISABLE, valid always
-    
+  // GLOBAL CORRECTION ENABLE
 
+  bool Enable_Correction(bool enable); // GLOBAL ENABLE or DISABLE, valid always
 
-    //FINAL COMPUTATIONS
-    double dE(const double& z, const double& tof);
-    double z_squared(double dE, double tof);
-
+  // FINAL COMPUTATIONS
+  double dE(const double &z, const double &beta);
+  double z_squared(double dE, double beta);
 };
-
-
 
 #endif
